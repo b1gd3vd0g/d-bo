48d8737fde2d11829d3cdbd6769d013c463ca389
b1gd3vd0g <devin.peevy@outlook.com>
2025-08-27 10:47:20 -0700

    - create the connection to the mongodb database   - (it connects to the database specified in the MONGO_DBNAME environment variable) - ping the database to ensure our connection can be made

---
+++ backend/src/mongo.rs
@@ -0,0 +1,48 @@
+use std::env;
+
+use mongodb::{Client, Database, bson::doc};
+use serde::{Deserialize, Serialize};
+use urlencoding::encode;
+
+#[derive(Deserialize, Serialize)]
+struct PingCounter {
+    pings: u32,
+}
+
+pub async fn d_bo_client() -> Database {
+    let mongo_username = env::var("MONGO_USERNAME")
+        .expect(r#"Environment variable "MONGO_USERNAME" is not configured."#);
+    let mongo_password = env::var("MONGO_PASSWORD")
+        .expect(r#"Environment variable "MONGO_PASSWORD" is not configured."#);
+    let mongo_server = env::var("MONGO_SERVER")
+        .expect(r#"Environment variable "MONGO_SERVER" is not configured."#);
+    let mongo_dbname = env::var("MONGO_DBNAME")
+        .expect(r#"Environment variable "MONGO_DBNAME" is not configured."#);
+
+    let mongo_uri = format!(
+        "mongodb+srv://{}:{}@{}/?retryWrites=true&w=majority&tls=true",
+        mongo_username,
+        encode(&mongo_password),
+        mongo_server
+    );
+
+    let mongo_client = Client::with_uri_str(mongo_uri)
+        .await
+        .expect("The mongo_uri string is malformed.");
+
+    // This is what we will return from the function to be used as an axum state.
+    let mongo_database = mongo_client.database(&mongo_dbname);
+
+    let filter = doc! { "name": "pings" };
+    let update = doc! { "$inc": { "pings": 1 } };
+
+    // Test to make sure that the connection works.
+    let _ping = mongo_database
+        .collection::<PingCounter>("pings")
+        .find_one_and_update(filter, update)
+        .upsert(true)
+        .await
+        .expect("Failed to connect to the mongodb database.");
+
+    mongo_database
+}
