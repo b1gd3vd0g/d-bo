e98741e1d783d589199ec11152a2da0211f1c9e9
b1gd3vd0g <devin.peevy@outlook.com>
2025-08-28 13:12:18 -0700

    - configure the router to perform the `handle_player_registration` functoin at the endpoint `POST /player`

--- backend/src/router.rs
+++ backend/src/router.rs
@@ -1,6 +1,8 @@
-use axum::Router;
+use axum::{Router, routing::post};
 use mongodb::Database;
 
+use crate::handlers::player::register::handle_player_registration;
+
 pub fn router() -> Router<Database> {
-    Router::new()
+    Router::new().route("/player", post(handle_player_registration))
 }
