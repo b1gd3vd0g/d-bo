57fd4a28538cfa5f781689ceb5f0ca2cc625037a
b1gd3vd0g <devin.peevy@outlook.com>
2025-08-28 12:37:51 -0700

    - Change the player_id field to a string, to be compatible with bson - add the confirmed boolean, to tell whether or not the email address has been verified yet

--- backend/src/mongo/models.rs
+++ backend/src/mongo/models.rs
@@ -1,5 +1,4 @@
 use serde::{Deserialize, Serialize};
-use uuid::Uuid;
 
 /// This is a counter of how many times we have tested our connection to the mongodb database.
 #[derive(Deserialize, Serialize)]
@@ -10,8 +9,9 @@ pub struct PingCounter {
 /// This is a player document in the database.
 #[derive(Deserialize, Serialize)]
 pub struct Player {
-    pub player_id: Uuid,
+    pub player_id: String,
     pub username: String,
     pub password: String,
     pub email: String,
+    pub confirmed: bool,
 }
