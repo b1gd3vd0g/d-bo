96c17696a4f8a73fb8f5afe33adc27bf12cbf0f9
b1gd3vd0g <devin.peevy@outlook.com>
2025-08-28 21:30:34 -0700

    - make getters for all Player struct fields - make Player::new() function

--- backend/src/mongo/models/player.rs
+++ backend/src/mongo/models/player.rs
@@ -1,1 +1,39 @@
+use crate::mongo::models::Player;
 
+impl Player {
+    pub fn new(
+        player_id: &str,
+        username: &str,
+        password: &str,
+        email: &str,
+        confirmed: bool,
+    ) -> Self {
+        Self {
+            player_id: String::from(player_id),
+            username: String::from(username),
+            password: String::from(password),
+            email: String::from(email),
+            confirmed: confirmed,
+        }
+    }
+
+    pub fn player_id(&self) -> String {
+        self.player_id.clone()
+    }
+
+    pub fn username(&self) -> String {
+        self.username.clone()
+    }
+
+    pub fn password(&self) -> String {
+        self.password.clone()
+    }
+
+    pub fn email(&self) -> String {
+        self.email.clone()
+    }
+
+    pub fn confirmed(&self) -> bool {
+        self.confirmed
+    }
+}
