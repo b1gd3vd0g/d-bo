43c35938bf5d080d8832526eee6bc5add845c600
b1gd3vd0g <devin.peevy@outlook.com>
2025-08-30 08:57:48 -0700

    - Add DBoError enum `RelationalConflict`

--- backend/src/errors.rs
+++ backend/src/errors.rs
@@ -7,6 +7,10 @@ pub enum DBoError {
     /// An email could not be sent to the player, **very likely** (but not guaranteed) because the
     /// provided email address does not exist.
     NonexistentEmail,
+    /// A document cannot be created, because it conflicts with the current state of the database.
+    /// For example, this could happen when a confirmation token is created, but it does not
+    /// correspond with a valid, active player account.
+    RelationalConflict(String),
     /// A server-side error has occurred. These are very unlikely, but not impossible. The String
     /// should contain a brief description of what went wrong. These strings should NOT be passed to
     /// the user within the HTTP request body, but instead should be logged using the eprintln!
