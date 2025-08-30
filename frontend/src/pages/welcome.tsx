98802eaf1e6d294f554f304376cc4c7308faceb6
b1gd3vd0g <devin.peevy@outlook.com>
2025-08-30 11:33:25 -0700

    - Give the player the option to log in or create a new account on the welcome page - Depending on their choice, open up the proper form - The forms are not functional yet; they simply include text fields where the required input should go

--- frontend/src/pages/welcome.tsx
+++ frontend/src/pages/welcome.tsx
@@ -1,18 +1,85 @@
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { FormGroup } from '../reusable/form';
+import { Box } from '../reusable/containers';
+
+type UserChoice = 'undecided' | 'login' | 'register';
+
+interface WelcomeFormProps {
+  setChoice: React.Dispatch<React.SetStateAction<UserChoice>>;
+}
 
 export default function WelcomePage() {
-  const navigate = useNavigate();
+  const [choice, setChoice] = useState<UserChoice>('undecided');
+  let form;
+  switch (choice) {
+    case 'undecided':
+      form = <ChoiceForm setChoice={setChoice} />;
+      break;
+    case 'login':
+      form = <LoginForm setChoice={setChoice} />;
+      break;
+    case 'register':
+      form = <RegisterForm setChoice={setChoice} />;
+      break;
+  }
+
   return (
     <>
       <h1>Welcome to D-Bo!</h1>
-      <button
-        onClick={() => {
-          sessionStorage.setItem('token', '123');
-          navigate('/');
-        }}
-      >
+      <Box className='m-auto'>{form}</Box>
+    </>
+  );
+}
+
+function ChoiceForm({ setChoice }: WelcomeFormProps) {
+  return (
+    <>
+      <button className='block' onClick={() => setChoice('login')}>
         Log in
       </button>
+      <button className='block' onClick={() => setChoice('register')}>
+        Create a new account
+      </button>
+    </>
+  );
+}
+
+function LoginForm({ setChoice }: WelcomeFormProps) {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  return (
+    <>
+      <button
+        className='bg-transparent text-3xl'
+        onClick={() => setChoice('undecided')}
+      >
+        {'<'}
+      </button>
+      <h4 className='text-center'>Login</h4>
+      <FormGroup label='Username/Email' setter={setUsername} />
+      <FormGroup label='Password' setter={setPassword} />
+    </>
+  );
+}
+
+function RegisterForm({ setChoice }: WelcomeFormProps) {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+
+  return (
+    <>
+      <button
+        className='bg-transparent text-3xl'
+        onClick={() => setChoice('undecided')}
+      >
+        {'<'}
+      </button>
+      <h4 className='text-center'>Create a new account</h4>
+      <FormGroup label='Username' setter={setUsername} />
+      <FormGroup label='Password' setter={setPassword} />
+      <FormGroup label='Email address' setter={setEmail} />
     </>
   );
 }
