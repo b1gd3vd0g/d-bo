b857d12d11ba2bfde9470ecd5d2d0ae6850c7368
b1gd3vd0g <devin.peevy@outlook.com>
2025-08-30 17:33:30 -0700

    - expose `mongo::models::player::confirm` module - implement `Player::confirm(..)` function, to find a player in the database and change `confirmed` to true

--- backend/src/mongo/models/player.rs
+++ backend/src/mongo/models/player.rs
@@ -1,3 +1,4 @@
+pub mod confirm;
 pub mod delete;
 pub mod find;
 pub mod register;
---
+++ backend/src/mongo/models/player/confirm.rs
@@ -0,0 +1,26 @@
+use mongodb::{Database, bson::doc};
+
+use crate::{errors::DBoError, mongo::models::Player};
+
+impl Player {
+    pub async fn confirm(db: &Database, player_id: &str) -> Result<(), DBoError> {
+        let confirmation = db
+            .collection::<Self>("players")
+            .find_one_and_update(
+                doc! { "player_id": player_id },
+                doc! { "$set": { "confirmed": true } },
+            )
+            .await;
+
+        match confirmation {
+            Ok(option) => match option {
+                Some(_) => Ok(()),
+                None => Err(DBoError::MissingDocument),
+            },
+            Err(e) => {
+                eprintln!("{:?}", e);
+                Err(DBoError::mongo_driver_error())
+            }
+        }
+    }
+}
