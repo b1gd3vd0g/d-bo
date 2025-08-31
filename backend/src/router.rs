e6faa9646d885f13fb8f928d250cfae4a2c51e5b
b1gd3vd0g <devin.peevy@outlook.com>
2025-08-31 08:22:27 -0700

    - route `POST /confirmation/{token_id}` to `handle_token_confirmation` - route `DELETE /confirmation/{token_id}` to `handle_token_rejection`

--- backend/src/router.rs
+++ backend/src/router.rs
@@ -1,8 +1,16 @@
 use axum::{Router, routing::post};
 use mongodb::Database;
 
-use crate::handlers::player::register::handle_player_registration;
+use crate::handlers::{
+    confirmation::{handle_token_confirmation, handle_token_rejection},
+    player::register::handle_player_registration,
+};
 
 pub fn router() -> Router<Database> {
-    Router::new().route("/player", post(handle_player_registration))
+    Router::new()
+        .route("/player", post(handle_player_registration))
+        .route(
+            "/confirmations/{token_id}",
+            post(handle_token_confirmation).delete(handle_token_rejection),
+        )
 }
