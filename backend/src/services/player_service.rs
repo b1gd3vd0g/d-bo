80b6d470002d8fb5754971d69d7a5c51a4ac0570
b1gd3vd0g <devin.peevy@outlook.com>
2025-09-07 14:18:16 -0700

    FEAT - implement `PlayerService::login(..)` function - add doc comments to it - small fix for other doc comments

--- backend/src/services/player_service.rs
+++ backend/src/services/player_service.rs
@@ -1,10 +1,16 @@
 //! This module handles all services related to **player accounts**.
 
 use crate::{
-    adapters::{email::send_confirmation_email, repositories::Repository},
-    errors::DBoResult,
+    adapters::{
+        email::send_confirmation_email,
+        hashing::{generate_secret, verify_secret},
+        jwt::generate_access_token,
+        repositories::Repository,
+    },
+    errors::{DBoError, DBoResult},
     handlers::responses::SafePlayerResponse,
-    models::{ConfirmationToken, Identifiable, Player},
+    models::{ConfirmationToken, Identifiable, Player, RefreshToken},
+    services::types::LoginTokenInfo,
 };
 
 pub struct PlayerService {}
@@ -19,6 +25,9 @@ impl PlayerService {
     /// - `username`: The requested username
     /// - `password`: The requested password
     /// - `email`: The requested email address
+    ///
+    /// ### Returns
+    /// The created player's safe information.
     ///
     /// ### Errors
     /// - `InvalidPlayerInfo` if the username, password, or email cannot pass validation.
@@ -43,5 +52,53 @@ impl PlayerService {
         send_confirmation_email(email, username, token.id()).await?;
 
         Ok(SafePlayerResponse::from(&player))
+    }
+
+    /// Attempt to verify a player's login information. Create an access token and a refresh token
+    /// for secure authentication. Store the refresh token in the database.
+    ///
+    /// ### Arguments
+    /// - `players`: The player repository
+    /// - `tokens`: The refresh token repository
+    /// - `username_or_email`: The player's username or email address
+    /// - `password`: The player's password
+    ///
+    /// ### Returns
+    /// The information related to the created authentication tokens
+    ///
+    /// ### Errors
+    /// - `AuthenticationFailure` if the username/email and password do not match our records
+    /// - `AdapterError(Database)` if a database query fails
+    /// - `AdapterError(Hashing)` if the password or refresh token secret cannot be hashed
+    /// - `AdapterError(Jwt)` if the access token cannot be created
+    pub async fn login(
+        players: &Repository<Player>,
+        tokens: &Repository<RefreshToken>,
+        username_or_email: &str,
+        password: &str,
+    ) -> DBoResult<LoginTokenInfo> {
+        let option = players.find_by_username_or_email(username_or_email).await?;
+
+        let player = if let Some(p) = option {
+            p
+        } else {
+            return Err(DBoError::AuthenticationFailure);
+        };
+
+        if !verify_secret(password, player.password())? {
+            return Err(DBoError::AuthenticationFailure);
+        }
+
+        let access_token = generate_access_token(player.id())?;
+        let refresh_secret = generate_secret();
+
+        let refresh_token = RefreshToken::new(player.id(), &refresh_secret)?;
+        tokens.insert(&refresh_token).await?;
+
+        Ok(LoginTokenInfo::new(
+            &access_token,
+            refresh_token.id(),
+            &refresh_secret,
+        ))
     }
 }
